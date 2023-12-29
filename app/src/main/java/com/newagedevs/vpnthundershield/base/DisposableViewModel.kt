9ba07a17a2abcb35fc42d77a9ead7f0d7a9d078b
imammasleap <imam@masleap.io>
2023-12-29 20:00:42 +0600

    development started

--- app/src/main/java/com/newagedevs/vpnthundershield/base/DisposableViewModel.kt
+++ app/src/main/java/com/newagedevs/vpnthundershield/base/DisposableViewModel.kt
@@ -1,14 +1,10 @@
 package com.newagedevs.vpnthundershield.base
 
 import com.skydoves.bindables.BindingViewModel
-import com.skydoves.sandwich.disposables.CompositeDisposable
 
 abstract class DisposableViewModel : BindingViewModel() {
-
-    val disposables: CompositeDisposable = CompositeDisposable()
-
   override fun onCleared() {
     super.onCleared()
-    disposables.clear()
+
   }
 }
